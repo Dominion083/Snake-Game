a3bd24c256b39c014be797056dc76b9d93435494
Dominion083 <93716182+Dominion083@users.noreply.github.com>
2023-12-18 07:28:21 +0000

    Author: Dominion Aromolaran - psyda5 Date: 18th December. Commit Type: Refactoring; Chore ; Fix
    Ehancing Start Class
    
    Making Start Class extend
    Applicatio to support JavaFX.
    
    Converted Wholly to JavaFX.
    
    Handled Key Inputs

--- src/main/java/comp2013psyda5/Start.java
+++ src/main/java/comp2013psyda5/Start.java
@@ -8,7 +8,7 @@ import javafx.scene.Scene;
 import java.io.IOException;
 
 
-public class Start {
+public class Start extends Application{
     /*@Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Start.class.getResource("hello-view.fxml"));
@@ -18,16 +18,28 @@ public class Start {
         stage.show();
     }
 
-    public static void main (String[]args)
-    {
-     launch();
-    }*/
+ */
+    @Override
+    public void start(Stage stage) throws Exception {
+        Play view = new Play();
+        GameFrame controller = new GameFrame();
+        Scene scene = new Scene(view, 870, 560);
+        stage.setScene(scene);
+
+        scene.setOnKeyPressed(event -> controller.handleKeyPressed(event,view.mySnake));
+
+        stage.setTitle("Snake Game");
+        stage.show();
+        MusicPlayer musicPlayer = new MusicPlayer("/music/frogger.mp3", true);
+        musicPlayer.play();
+
+    }
 
     public static void main(String[] args)
     {
-        new Play().loadFrame();
-        new MusicPlayer("src/example/frogger.mp3",true);
 
+        launch();
     }
+
 
 }
