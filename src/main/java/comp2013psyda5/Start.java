a9ff8778b88b23fe6f020e6884ffb30a3e0da6be
Dominion083 <93716182+Dominion083@users.noreply.github.com>
2023-12-19 20:48:35 +0000

    Author: Dominion Aromolaran - psyda5 Date: 19th December. Commit Type: Chore ; Fix
    Updated Start Class
    
    Replaced Magic Numbers.
    
    Implement Snake Game icon in JavaFX.

--- src/main/java/comp2013psyda5/Start.java
+++ src/main/java/comp2013psyda5/Start.java
@@ -2,6 +2,8 @@ package comp2013psyda5;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
@@ -23,15 +25,15 @@ public class Start extends Application{
     public void start(Stage stage) throws Exception {
         Play view = new Play();
         GameFrame controller = new GameFrame();
-        Scene scene = new Scene(view, 870, 560);
+        Scene scene = new Scene(view, Constants.GAME_WIDTH, Constants.GAME_HEIGHT);
         stage.setScene(scene);
-
+        Image icon = GameImageLoader.getImages().get("snake-icon");
+        stage.getIcons().add(icon);
         scene.setOnKeyPressed(event -> controller.handleKeyPressed(event,view.mySnake));
-
         stage.setTitle("Snake Game");
         stage.show();
         MusicPlayer musicPlayer = new MusicPlayer("/music/frogger.mp3", true);
-        musicPlayer.play();
+
 
     }
 
