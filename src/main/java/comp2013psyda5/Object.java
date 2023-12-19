aced8a711e3737e1bece34cabebc28c198f2ba35
Dominion083 <93716182+Dominion083@users.noreply.github.com>
2023-12-19 20:46:55 +0000

    Author: Dominion Aromolaran - psyda5 Date: 19th December. Commit Type: Chore ; Fix
    Ehanced Object Class
    
    Added Implementation for draw
    function.

--- src/main/java/comp2013psyda5/Object.java
+++ src/main/java/comp2013psyda5/Object.java
@@ -4,16 +4,18 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.geometry.Rectangle2D;
 
-public abstract class SnakeObject {
+public abstract class Object {
+
     int x;
     int y;
     Image image;
     int width;
     int height;
-    public boolean isAlive;
 
 
-    public abstract void draw(GraphicsContext gc);
+    public void draw(GraphicsContext gc){
+        gc.drawImage(image, x, y);
+    };
 
     public Rectangle2D getRectangle() {
         return new Rectangle2D(x, y, width, height);
