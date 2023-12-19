ac6be1518910ef2498405f598f055b3e639e5712
Dominion083 <93716182+Dominion083@users.noreply.github.com>
2023-12-19 20:52:56 +0000

    Author: Dominion Aromolaran - psyda5 Date: 19th December. Commit Type: Chore ; Fix
    Updated Play(View) Class
    
    Replaced Magic Numbers.
    
    Added Image View for explosion
    animation.
    
    Implemented  delay between explosion
    animation and end game screen

--- src/main/java/comp2013psyda5/Play.java
+++ src/main/java/comp2013psyda5/Play.java
@@ -1,14 +1,18 @@
 package comp2013psyda5;
 
 import javafx.animation.AnimationTimer;
+import javafx.animation.PauseTransition;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.util.Duration;
+
 
 public class Play extends Pane {
 	private Canvas canvas;
@@ -16,23 +20,33 @@ public class Play extends Pane {
 	private FoodModel foodModel;
 	private Image background;
 	private Image fail;
-    private GameFrame controller;
+	private Image boom;
+	private ImageView explosionView;
+	private GameFrame controller;
+
+	private boolean endScreenShown = false;
+
 
 
 	public Play() {
 		controller = new GameFrame();
 		foodModel = new FoodModel();
 		mySnake = new MySnake(100, 100); // Initialize with starting position
-		background = GameImageLoader.getImages().get("UI-background");
-		fail = GameImageLoader.getImages().get("game-scene-01");
+		background = GameImageLoader.getImages().get("background");
+		fail = GameImageLoader.getImages().get("endgame");
+		boom = new Image(getClass().getResourceAsStream("/images/boom.gif"));
 
 		canvas = new Canvas(870, 560); // Set canvas size
-		getChildren().add(canvas);
+		explosionView = new ImageView(boom);
+		explosionView.setFitWidth(40);  // Set the new width
+		explosionView.setFitHeight(40); // Set the new height
+		explosionView.setPreserveRatio(true);
+		explosionView.setVisible(false);
+
+		getChildren().addAll(canvas,explosionView);
 
 		// Set up key event handling
 		setFocusTraversable(true);
-
-
 
 		// Start game loop
 		startGameLoop();
@@ -49,15 +63,19 @@ public class Play extends Pane {
 			}
 		}.start();
 	}
-
+	public ImageView getImageView(){
+		return explosionView;
+	}
 
 
 	public void draw() {
 		GraphicsContext gc = canvas.getGraphicsContext2D();
-		gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight()); // Clear the canvas
-		gc.drawImage(background, 0, 0);
+
 
 		if (mySnake.isAlive()) {
+			gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight()); // Clear the canvas
+			gc.drawImage(background, 0, 0);
+			endScreenShown = false;
 			mySnake.draw(gc);
 			if (!foodModel.isEaten()) {
 				foodModel.draw(gc);
@@ -65,11 +83,26 @@ public class Play extends Pane {
 			}
 			else{
 				foodModel = new FoodModel();
+				MusicPlayer musicPlayer = new MusicPlayer("/music/munch.mp3", false);
+
 			}
-		} else {
-			gc.drawImage(fail, 0, 0);
+		} else if (!endScreenShown){
+			explosionView.setX(mySnake.getX());
+			explosionView.setY(mySnake.getY());
+			explosionView.setVisible(true);
+			MusicPlayer musicPlayer = new MusicPlayer("/music/explosion.mp3", true);
+			PauseTransition pause = new PauseTransition(Duration.seconds(2));
+			pause.setOnFinished(event -> showEndScreen(gc));
+			pause.play();
+			endScreenShown = true;
 		}
+
 		drawScore(gc,mySnake);
+	}
+	private void showEndScreen(GraphicsContext gc) {
+		gc.drawImage(fail,0,0,canvas.getWidth(),canvas.getHeight());
+		explosionView.setVisible(false);
+
 	}
 
 	private void drawScore(GraphicsContext gc,MySnake mySnake) {
