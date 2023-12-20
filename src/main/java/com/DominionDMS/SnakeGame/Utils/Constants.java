a80754e991ef53413a09d5b22020f8f38765aeed
Dominion083 <93716182+Dominion083@users.noreply.github.com>
2023-12-20 11:14:36 +0000

    Author: Dominion Aromolaran - psyda5 Date: 20th November. Commit Type: Chore, Enhancements;
    Refactoring MVC architecture:

--- src/main/java/com/DominionDMS/SnakeGame/Utils/Constants.java
+++ src/main/java/com/DominionDMS/SnakeGame/Utils/Constants.java
@@ -4,11 +4,13 @@ public class Constants {
 
         public static final int GAME_WIDTH = 780;
         public static final int GAME_HEIGHT = 560;
-       /* public static final int BOOM_HEIGHT = 40;
-        public static final int BOOM_WIDTH = 40;*/
-        public static final int SNAKE_STARTX = 100;
+        public static final int BOOM_HEIGHT = 40;
+        public static final int BOOM_WIDTH = 40;
+        public static final int SNAKE_START_X = 100;
+        public static final int SNAKE_START_Y = 100;
+        public static final int SNAKE_SCORE_X= 20;
+        public static final int SNAKE_SCORE_Y= 40;
 
-        public static final int SNAKE_STARTY= 100;
 
 
 
